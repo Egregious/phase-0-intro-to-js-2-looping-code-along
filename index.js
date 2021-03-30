df917a27a9622f51d508b87d1d7fcad39aa04603
Egregious <dvdscheibel@yahoo.com>
2021-03-30 00:03:47 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solutions in this file
+
+const names = ['Ada', 'Brendan', 'Ali'];
+
+const event = "birthday";
+
+const messages = [];
+
+function writeCards(names, event) {
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    console.log(messages);
+    return messages;
+}
+
+function countDown(number) {
+    let i = 0;
+    while (number >= 0) {
+        i++;
+        console.log(number);
+        number--;
+    }
+    return i;
+}
+console.log(countDown(10));
+console.log(countDown(4));
